2f2a1da2902f255f7b0fee744259adf01d1eecfb
MaksaNeNegr <maksazzw@gmail.com>
2025-03-06 01:18:29 +0900

    helppp

--- demon/demon.go
+++ demon/demon.go
@@ -3,13 +3,13 @@ package demon
 import(
 	"fmt"
 	"time"
-	"github.com/MaksaNeNegr/calc_go/godotenv"
+	"github.com/MaksaNeNegr/calc_go/vars"
 	"os"
 )
 
 
 func Demon_func(){
-	err := godotenv.Load()
+	vars.Load()
 	for{
 		fmt.Println(os.Getenv("GITHUB_USERNAME"))
 		time.Sleep(2 * time.Second)
